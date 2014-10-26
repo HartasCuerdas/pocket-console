d69ad28c2def909c2d47e3e47395e07cf78e9e26
Franco Cedillo <franco.cedillo@gmail.com>
2014-10-26 00:09:39 -0500

    adds post-install message, and better format

--- lib/tagStats.rb
+++ lib/tagStats.rb
@@ -12,36 +12,36 @@ class TagStats
 
   def printMostUsed
     @tags = @tags.sort_by{|_key, stats| stats['count']}.reverse!
-    puts '================================'
-    puts '     Tag Stats - Most Used      '
-    puts '--------------------------------'
-    puts sprintf "%-12s | %6s  | %7s", 'tag', 'total', 'unread'
+    puts '================================='
+    puts '      Tag Stats - Most Used      '
+    puts '---------------------------------'
+    puts sprintf "%-13s | %6s  | %7s", 'tag', 'total', 'unread'
     @tags.each do |tag, stats|
       count = stats['count']
       if count == 1
         break
       end
       unread = stats['unread']
-      puts sprintf "%-12s | %6d  | %7d", tag, count, unread
+      puts sprintf "%-13s | %6d  | %7d", tag, count, unread
     end
-    puts '================================'
+    puts '================================='
   end
 
   def printMostUnread
     @tags = @tags.sort_by{|_key, stats| stats['unread']}.reverse!
-    puts '================================'
-    puts '     Tag Stats - Most Unread    '
-    puts '--------------------------------'
-    puts sprintf "%-12s | %7s  | %6s", 'tag', 'unread', 'total'
+    puts '================================='
+    puts '     Tag Stats - Most Unread     '
+    puts '---------------------------------'
+    puts sprintf "%-13s | %7s  | %6s", 'tag', 'unread', 'total'
     @tags.each do |tag, stats|
       unread = stats['unread']
       if unread == 1
         break
       end
       count = stats['count']
-      puts sprintf "%-12s | %7d  | %6d", tag, unread, count
+      puts sprintf "%-13s | %7d  | %6d", tag, unread, count
     end
-    puts '================================'
+    puts '================================='
   end
 
 end
--- pocket-console.gemspec
+++ pocket-console.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name        = 'pocket-console'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2014-10-25'
   s.summary     = 'Stats from Pocket on your console'
   s.add_runtime_dependency 'pocket-classes', '~> 0.0.1'
-  s.description = 'This gem provides methods to output stats about Pocket tags.'
+  s.description = 'Stats about Pocket tags, on your console.'
   s.authors     = ['Franco Cedillo']
   s.email       = 'franco.cedillo@gmail.com'
   s.files       = [
@@ -15,4 +15,5 @@ Gem::Specification.new do |s|
                   ]
   s.homepage    = 'http://rubygems.org/gems/pocket-console'
   s.license     = 'MIT'
+  s.post_install_message = 'Pocket Stats in your console'
 end
